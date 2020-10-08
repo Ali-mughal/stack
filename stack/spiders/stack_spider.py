5a13be6adeae519cc1fa03afa39f2e5a24c7167e
Ali Mughal <alie.mughal@gmail.com>
2020-10-08 19:40:57 +0400

    my first commit

---
+++ stack/spiders/stack_spider.py
@@ -0,0 +1,16 @@
+from scrapy import Spider
+from stack.items import StackItem
+
+class StackSpider(Spider):
+    name = "stack"
+    allowed_domains = ["stackoverflow.com"]
+    start_urls = ["http://stackoverflow.com/questions?pagesize=50&sort=newest"]
+    def parse(self, response):
+        title=response.css('div.summary >h3>a::text').getall()
+        urls=response.css('div.summary >h3>a::attr(href)').getall()
+        for title,url in zip(title,urls):
+            item = StackItem()
+            item['title'] = title
+            item['url'] = url
+            yield item
+
